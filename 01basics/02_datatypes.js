a9fcacfe2dfbb54f4e3828031ec1f515a8c20b3f
vanispurtree1111 <vani.kn@spurtreetech.com>
2024-02-06 23:32:13 +0530

    datatypes - D-2

---
+++ 01basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict";    //treat all Js code as newer version
+
+//alert ( 3+ 3)  // we are using nodejs, not browser
+
+console.log("vani");   //code readability should be high
+
+//datatypes
+
+let name = "string";  
+let age = 20;
+let isLoggedIn = true;
+
+//Primitive datatypes
+
+// number => 2 to power 53
+//bigInt
+//string   => ' '   or  " "
+//boolean => true, false
+//null   => standalone value  - representation of empty 
+//undefined => initialised but dint assign value
+//symbol => unique
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
+
+//object 
+
+
+
